14c25ce8642e73ac1607f961a10436ab75fecefa
Kim, Ji-Soo <kim@jisoo.net>
2017-01-07 21:52:14 +0900

    Update request to urllib

--- naver_book_query/NaverBookQuery.py
+++ naver_book_query/NaverBookQuery.py
@@ -1,4 +1,5 @@
 import copy
+import json
 import urllib.error
 import urllib.parse
 import urllib.request
@@ -18,58 +19,18 @@ class NaverBookQuery:
     __base_url = 'https://openapi.naver.com/v1/search/book_adv.json?'
 
     def __init__(self):
-        self.__offset = 1
-        self.__limit = 10
-        self.criterion = {}
-
-    @generative
-    def filter(self, *criterion):
-        self.criterion = {key: value for dic in criterion for key, value in dic.items()}
-
-    @generative
-    def offset(self, offset):
-        self.__offset = offset + 1
-
-    @generative
-    def limit(self, limit):
-        self.__limit = limit
-
-    def count(self):
-        pass
-
-    def get(self, isbn):
-        self.criterion['isbn'] = isbn
-        self.offset(0)
-        self.limit(1)
-        return self.__request()
-
-    def first(self):
-        self.__limit = 1
-        return self.__request()
-
-    def all(self):
-        return self.__request()
-
-    def __request(self):
-        if self.client_key is None or self.secret_key is None:
-            raise EnvironmentError('Undefined client_key or secret_key')
-        headers = {
-            'X-Naver-Client-Id': self.client_key,
-            'X-Naver-Client-Secret': self.secret_key,
+        self.criterion = {
+            'start': 1,
+            'display': 10,
         }
-        request = urllib.request.Request(self.__base_url + self.__get_param(),
-                                         headers=headers)
-        try:
-            response = urllib.request.urlopen(request)
-        except urllib.error.HTTPError as error:
-            pass
-        return response.read().decode('utf-8')
 
-    def __get_param(self):
+    @generative
+    def filter(self, **criterion):
+        self._filter(**criterion)
+
+    def _filter(self, **criterion):
         rename_table = {
             'query': 'query',
-            'offset': 'start',
-            'limit': 'display',
             'title': 'd_titl',
             'author': 'd_auth',
             'content': 'd_cont',
@@ -78,9 +39,51 @@ class NaverBookQuery:
             'publish_start': 'd_dafr',
             'publish_end': 'd_dato',
         }
-        self.criterion['offset'] = self.__offset
-        self.criterion['limit'] = self.__limit
-        naver_keyword = {}
-        for k in self.criterion.keys():
-            naver_keyword[rename_table[k]] = self.criterion[k]
-        return urllib.parse.urlencode(naver_keyword)
+        for key, value in criterion.items():
+            if key in rename_table:
+                self.criterion[rename_table[key]] = criterion[key]
+
+    def offset(self, offset):
+        if offset < 0 or offset > 999:
+            raise ValueError('Offset only supports between 0-999.')
+        self.criterion['start'] = offset + 1
+        return self
+
+    def limit(self, limit):
+        if limit < 1 or limit > 100:
+            raise ValueError('Limit only supports between 1-100.')
+        self.criterion['display'] = limit
+        return self
+
+    def count(self):
+        return self.__request()['display']
+
+    def get(self, isbn):
+        self._filter(isbn=isbn)
+        self.offset(0)
+        self.limit(1)
+        return self.__request()['items']
+
+    def first(self):
+        self.limit(1)
+        return self.__request()['items']
+
+    def all(self):
+        return self.__request()['items']
+
+    def __get_param(self):
+        return urllib.parse.urlencode(self.criterion)
+
+    def __request(self):
+        if self.client_key is None or self.secret_key is None:
+            raise EnvironmentError('Undefined client_key or secret_key')
+
+        headers = {
+            'X-Naver-Client-Id': self.client_key,
+            'X-Naver-Client-Secret': self.secret_key,
+        }
+        request = urllib.request.Request(self.__base_url + self.__get_param(),
+                                         headers=headers)
+        response = urllib.request.urlopen(request)
+
+        return json.loads(response.read().decode('utf-8'))
