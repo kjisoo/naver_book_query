ad938ded4fd2eeda8c731bb93545c80f3b4dd479
Kim, Ji-Soo <kim@jisoo.net>
2017-01-10 23:11:35 +0900

    Add docstring

--- book.py
+++ book.py
@@ -1,5 +1,29 @@
 class Book:
+    """
+    | The basic type of book model.
+    | Implemented __init__ to receive dict.
+
+    dict example
+
+    {
+    
+        | 'title': 'Title of the book.',
+        | 'link': 'hypertext link of the search result document.',
+        | 'image': 'The URL of the thumbnail image.',
+        | 'author': 'Author of the book',
+        | 'price': 'Price of the book',
+        | 'discount': 'Discount price of the book',
+        | 'publisher': 'Publisher of the book',
+        | 'isbn': 'List of isbn separated by spaces.',
+        | 'description': 'Summary of the contents of the search result book.',
+
+    }
+    """
     def __init__(self, data):
+        """
+
+        :param data:
+        """
         self.title = data.get('title').replace('<b>', '').replace('</b>', '')
         self.link = data.get('link')
         self.image_link = data.get('image')
--- naver_book_query.py
+++ naver_book_query.py
@@ -1,4 +1,5 @@
 import copy
+import functools
 import json
 import urllib.error
 import urllib.parse
@@ -6,6 +7,7 @@ import urllib.request
 
 
 def generative(f):
+    @functools.wraps(f)
     def wrapper(*args, **kwargs):
         self = copy.deepcopy(args[0])
         f(self, *args[1:], **kwargs)
@@ -14,6 +16,18 @@ def generative(f):
 
 
 class NaverBookQuery:
+    """Request a query using the api book of Naver.
+
+    Client_key and secret_key are required.
+
+    >>> NaverBookQuery.client_key = 'client key of naver'
+    >>> NaverBookQuery.secret_key = 'secret key of naver'
+
+    | The default type for the search is dict.
+    | If you want to change the default type, put a value in model_cls.
+    | The type must implement __init__ to receive the dict.
+    | See :class:`book` for details.
+    """
     client_key = None
     secret_key = None
     model_cls = None
@@ -29,11 +43,31 @@ class NaverBookQuery:
 
     @generative
     def filter(self, **criterion):
+        """Add to the filter by input criteria.
+
+        example:
+            >>> query = NaverBookQuery()
+
+            >>> query = query.filter(title ='title of the book')
+            >>> query = query.filter(author = 'me')
+            or
+            >>> query = query.filter(title='title of the book', author = 'me')
+
+        :param criterion:
+            | title : Title of the book
+            | author : Author of the book
+            | content : Table of Contents
+            | isbn : ISBN number of the book
+            | publisher : Book publisher
+            | publish_start : Publication date of the book
+            | publish_end : Publication end date of the book
+
+        :return: Copied object with filter applied
+        """
         self._filter(**criterion)
 
     def _filter(self, **criterion):
         rename_table = {
-            'query': 'query',
             'title': 'd_titl',
             'author': 'd_auth',
             'content': 'd_cont',
@@ -47,22 +81,54 @@ class NaverBookQuery:
                 self.criterion[rename_table[key]] = criterion[key]
 
     def offset(self, offset):
+        """offset of search, default 0
+
+        :param offset: Between 0-999
+        :return: self with offset applied
+        """
         if offset < 0 or offset > 999:
             raise ValueError('Offset only supports between 0-999.')
         self.criterion['start'] = offset + 1
         return self
 
     def limit(self, limit):
+        """display limit of search, default 10
+
+        :param limit: Between 1-100
+        :return: self with limit applied
+        """
         if limit < 1 or limit > 100:
             raise ValueError('Limit only supports between 1-100.')
         self.criterion['display'] = limit
         return self
 
     def count(self):
+        """Count of search results
+
+        :return: Count of search results
+        """
         self.__request()
         return self.response.get('display', 0)
 
     def get(self, isbn):
+        """Search for books by ISBN number.
+
+        Note that filters are applied, except for offset and limit.
+
+        example:
+            some book named 'Python' and isbn 10000
+
+            >>> query.get('10000')
+
+            result is book
+
+            >>> query.filter(title='java').get('10000')
+
+            result is None
+
+        :param str isbn: ISBN number of the book
+        :return: one object or none, default type is dict
+        """
         self._filter(isbn=isbn)
         self.offset(0)
         self.limit(1)
@@ -70,11 +136,21 @@ class NaverBookQuery:
         return self.__get_item_or_none()
 
     def first(self):
+        """First book in search results
+        Note that filters are applied, except for limit.
+
+        :return: one object or none, default type is dict
+        """
         self.limit(1)
         self.__request()
         return self.__get_item_or_none()
 
     def all(self):
+        """First book in search results
+        Note that filters are applied, except for limit.
+
+        :return: list of object or empty list, default type is list of dict
+        """
         self.__request()
         return self.__get_items()
 
