8d5c2174459b08ef7ab9587fdafbbc69d39a2698
Kim, Ji-Soo <kim@jisoo.net>
2017-01-11 22:53:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name="Naver-Book-Query",
-    version='0.9.0',
+    version='0.9.1',
     author='Kim jisoo',
     author_email='kim@jisoo.net',
     license='MIT License',
@@ -17,4 +17,9 @@ setup(
     description="Use naver book query like sqlalchemy",
     long_description=readme,
     install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
